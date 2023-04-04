f577fb7199bf2b7157825f9803725813ccaa343f
makwerik <serezha0790@mail.ru>
2023-04-04 18:37:13 +0300

    Добавил строку: engine.say("Привет, я голосовой ассистент!")

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ engine = pyttsx3.init()     # инициализация движка
 engine.setProperty('rate', 150)     # скорость речи
 engine.setProperty('volume', 0.9)   # громкость (0-1)
 
-engine.say("I can speak!")      # запись фразы в очередь
-engine.say("Я могу говорить!")  # запись фразы в очередь
+engine.say("Привет, я голосовой ассистент!")  # запись фразы в очередь
 
 # очистка очереди и воспроизведение текста
 engine.runAndWait()
