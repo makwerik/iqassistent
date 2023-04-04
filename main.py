de1cec7bdd4dbaf60217699ae871abba83052ced
makwerik <serezha0790@mail.ru>
2023-04-04 19:03:21 +0300

    Добавил функцию say_file, для прочтения текста из нужного файла

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ engine.setProperty('rate', 150)     # скорость речи
 engine.setProperty('volume', 0.9)   # громкость (0-1)
 
 engine.say("Привет, я голосовой ассистент! На данный момент я могу прочитать текст из файла, просто укажите мне путь до него")  # запись фразы в очередь
-input('Путь: ')
+
 # очистка очереди и воспроизведение текста
 engine.runAndWait()
+file = input(r'Путь: ')
+
+def say_file(file):
+    text_file = open(f"{file}", "r", encoding='utf8')
+    data = text_file.read()
+    engine.say(data)
+    engine.runAndWait()
+    text_file.close()
+
+if __name__ == '__main__':
+    say_file(file)
